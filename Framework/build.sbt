868bb1d24284218b640ccd9d21cb539827859289
Casper Siksma <casper@actinius.com>
2021-07-30 15:04:50 +0200

    Add functionality for calculating cost and computation time per partial QEP

--- Framework/build.sbt
+++ Framework/build.sbt
@@ -2,12 +2,18 @@ name := "framework"
 
 version := "0.1"
 
-scalaVersion := "2.11.12"
+//scalaVersion := "2.11.12"
+//
+//val sparkVersion = "2.4.5"
 
-val sparkVersion = "2.4.5"
+scalaVersion := "2.12.12"
+
+val sparkVersion = "3.1.1"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-hive" % sparkVersion,
   "au.com.bytecode" % "opencsv" % "2.4"
+//  "com.opencsv" % "opencsv" % "5.5.1"
 )
--- Framework/src/main/scala/Main.scala
+++ Framework/src/main/scala/Main.scala
@@ -2,45 +2,71 @@ import java.io.File
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Filter, Join, LogicalPlan, Project, ReturnAnswer, Sort, SubqueryAlias}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{AnalysisException, DataFrame, Row, SparkSession}
 
 import scala.collection.immutable.Map
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-
 import java.io.{BufferedWriter, FileWriter}
 import au.com.bytecode.opencsv.CSVWriter
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.{InternalRow, TableIdentifier}
 import org.apache.spark.sql.catalyst.analysis.EliminateSubqueryAliases
+import org.apache.spark.sql.catalyst.catalog.CatalogTableType
+import org.apache.spark.util.{SizeEstimator, Utils}
+
+import java.math.{MathContext, RoundingMode}
+import java.util.Locale
+import org.apache.spark.sql.execution.CostMode
+import org.apache.spark.sql.types.{BooleanType, DataType, IntegerType, StringType, StructField, StructType, TimestampType, NullType}
+import org.apache.spark.sql.execution.command.{AnalyzeTableCommand, ExplainCommand}
+import org.apache.spark.sql.internal.SQLConf.CBO_ENABLED
+
+import java.sql.Timestamp
+import java.util.concurrent.TimeUnit.NANOSECONDS
+import scala.util.Random
 
 object Main {
     val sparkSession: SparkSession = SparkSession.builder
         .appName("Taster")
+//        .enableHiveSupport()
         .master("local[*]")
         .getOrCreate();
+//    sparkSession.conf.set(CBO_ENABLED.key, value = true)
+    println("Using CBO: " + sparkSession.conf.get(CBO_ENABLED.key))
+    import sparkSession.implicits._
+    import sparkSession.sqlContext.implicits._
 
     val ROOT_LOGGER: Logger = Logger.getRootLogger
     ROOT_LOGGER.setLevel(Level.ERROR)
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-    val DATA_SET: String = "phonelabs" // "skyserver" | "phonelabs" | "iot"
+    val DATA_SET: String = "iot" // "skyserver" | "phonelabs" | "iot"
     val DATA_DIR: String = "/Users/falker/Documents/Projects/Deep-Learning-For-SQL-Operators/Framework/data/";
     val LOG_DIR: String = DATA_DIR + DATA_SET + "/logs/";
     val OUTPUT_DIR: String = DATA_DIR + DATA_SET + "/output/";
     val SCHEMA_DIR: String = DATA_DIR + DATA_SET + "/schema/";
     val USE_DATA_SUBSET: Boolean = false;
-    val TOP_K = 5000;
+    val TOP_K = 6000;
+    val FILL_DUMMY_DATA: Boolean = true;
+    val NUM_DUMMY_ROWS = 1000000;
 
     val tableCounter: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()
     var subTreeCount: mutable.HashMap[LogicalPlan, Long] = new mutable.HashMap[LogicalPlan, Long]()
     var subTreeIds: mutable.HashMap[LogicalPlan, Long] = new mutable.HashMap[LogicalPlan, Long]()
+    var subTreeComputationTime: mutable.HashMap[Long, Long] = new mutable.HashMap[Long, Long]()
     var subTreeDependencyGraph: mutable.HashMap[LogicalPlan, Set[LogicalPlan]] = new mutable.HashMap[LogicalPlan, Set[LogicalPlan]]()
     var subTreeIdsDependencyGraph: mutable.HashMap[Long, Set[Long]] = new mutable.HashMap[Long, Set[Long]]()
     var dataframeSizes: Map[String, Int] = Map[String, Int]()
 
     def main(args: Array[String]): Unit = {
         val dataframes: Map[String, DataFrame] = loadSchema()
+
+//        println("Analyzing tables...")
+//        analyzeTables(dataframes)
+
         val queries: List[String] = DATA_SET match {
             case "skyserver" => queryWorkloadSkyserver()
             case "phonelabs" => queryWorkloadPhoneLabs()
@@ -64,6 +90,8 @@ object Main {
         createTreeIds()
 
         createTreeIdsDependencyGraph()
+
+        analyzeSubTreeComputationTime()
 
         val encodedQueries: Seq[Set[Long]] = encodeQueries(processedQueries)
 
@@ -115,13 +143,54 @@ object Main {
      */
     def createTreeIdsDependencyGraph(): Unit = {
         subTreeDependencyGraph foreach {
-            case (lp, subTreeSet) => {
+            case (lp, subTreeSet) =>
                 if (subTreeIds.contains(lp)) {
                     val subTreeIdsSet = subTreeSet.filter(p => subTreeIds.contains(p)).map(p => subTreeIds(p))
                     subTreeIdsDependencyGraph(subTreeIds(lp)) = subTreeIdsSet
                 }
-            }
         }
+    }
+
+    /**
+     * Evaluates each unique partial QEP in subTreeIds to obtain the running times
+     */
+
+    def analyzeSubTreeComputationTime(): Unit = {
+        subTreeIds foreach {
+            case (lp, id) =>
+                val start = System.nanoTime()
+                sparkSession.sessionState.executePlan(lp).toRdd
+                val end = System.nanoTime()
+                println("========== Running Partial QEP ==========")
+                println(lp)
+                val duration = NANOSECONDS.toMillis(end - start)
+                println(s"Time taken: ${duration} ms")
+                subTreeComputationTime(id) = duration
+        }
+    }
+
+    def randomStringGen(length: Int): String = scala.util.Random.alphanumeric.take(length).mkString
+
+    def randomRowBySchema(schema: StructType): Seq[Any] = {
+        val types = schema.map((f: StructField) => {
+            if (f.name != "deleted_at") {
+                f.dataType
+            } else {
+                NullType
+            }
+        })
+        val dummyrow = types map {
+            case IntegerType => Random.nextInt(NUM_DUMMY_ROWS / 1000)
+//            case StringType => randomStringGen(5)
+//            case IntegerType => 0
+            case StringType => "0"
+//            case TimestampType => "2014-08-11 07:46:59".asInstanceOf[TimestampType]
+            case TimestampType => new Timestamp(System.currentTimeMillis());
+            case BooleanType => Random.nextBoolean()
+            case NullType => null
+            case _ => "0"
+        }
+        dummyrow
     }
 
     /**
@@ -150,15 +219,23 @@ object Main {
                 val filename: String = files(i).getName.substring(0, files(i).getName.lastIndexOf('.')).capitalize
                 println("Reading: " + files(i).getName)
                 try {
-                    val loaded: DataFrame = sparkSession.sqlContext
+                    var loaded: DataFrame = sparkSession.sqlContext
                         .read.format("com.databricks.spark.csv")
                         .option("header", "true")
                         .option("inferSchema", "true")
                         .option("delimiter", delimiter)
                         .option("nullValue", "null")
                         .load(SCHEMA_DIR + "/" + files(i).getName)
-                    dataframes += (filename -> loaded)
-                    sparkSession.sqlContext.createDataFrame(loaded.rdd, loaded.schema).createOrReplaceTempView(filename)
+
+                    if (FILL_DUMMY_DATA) {
+                        val lstrows = Seq.fill(NUM_DUMMY_ROWS)(randomRowBySchema(loaded.schema)).toList.map { x => Row(x: _*) }
+//                        .map { x => Row(x: _*) }
+                        val rdd = sparkSession.sparkContext.parallelize(lstrows)
+                        val dummy_df = sparkSession.createDataFrame(rdd, loaded.schema)
+                        loaded = loaded.union(dummy_df)
+                        dataframes += (filename -> loaded)
+                        sparkSession.createDataFrame(loaded.rdd, loaded.schema).createOrReplaceTempView(filename)
+                    }
                 } catch {
                     case e: Exception =>
                         println("Error occurred for: " + files(i).getName)
@@ -166,14 +243,27 @@ object Main {
                 }
             }
         }
-        sparkSession.catalog.listTables()
+
+        sparkSession.catalog.listTables("default").foreach(t => println(t))
+
         dataframes.keys.foreach((k) => {
             sparkSession.table(k).printSchema()
+            sparkSession.table(k).cache().count()
+
+            sparkSession.table(k).explain(mode="cost")
+
+//            AnalyzeTableCommand(TableIdentifier(k), noscan = false).run(sparkSession)
+
+//            println(sparkSession.sessionState.catalog.tableExists(TableIdentifier("users")))
+//            println(sparkSession.catalog.tableExists("users"))
+
             println(k, sparkSession.table(k).count())
+            println(sparkSession.table(k).head(5).mkString("Array(", ", ", ")"))
         })
         dataframeSizes = dataframes.map(pair => (pair._1, pair._2.columns.length))
         dataframes
     }
+
 
     /**
      * Loads the query log from the phonelabs files into a list of usable SQL queries
@@ -250,6 +340,7 @@ object Main {
                 val rawPlan: LogicalPlan = sparkSession.sqlContext.sql(query).queryExecution.analyzed
                 println(rawPlan)
                 println("========== logicalPlan ==========")
+                //Same as the optimized plan
                 val logicalPlan: LogicalPlan = sparkSession.sessionState.optimizer.execute(rawPlan)
                 val afterEliminateSubqueryAliases = EliminateSubqueryAliases(logicalPlan)
                 println(afterEliminateSubqueryAliases)
@@ -289,6 +380,53 @@ object Main {
         encodedQueries.toSeq
     }
 
+
+    def bytesToString(size: Long): String = bytesToString(BigInt(size))
+
+    def bytesToString(size: BigInt): String = {
+        val EB = 1L << 60
+        val PB = 1L << 50
+        val TB = 1L << 40
+        val GB = 1L << 30
+        val MB = 1L << 20
+        val KB = 1L << 10
+
+        if (size >= BigInt(1L << 11) * EB) {
+            // The number is too large, show it in scientific notation.
+            BigDecimal(size, new MathContext(3, RoundingMode.HALF_UP)).toString() + " B"
+        } else {
+            val (value, unit) = {
+                if (size >= 2 * EB) {
+                    (BigDecimal(size) / EB, "EB")
+                } else if (size >= 2 * PB) {
+                    (BigDecimal(size) / PB, "PB")
+                } else if (size >= 2 * TB) {
+                    (BigDecimal(size) / TB, "TB")
+                } else if (size >= 2 * GB) {
+                    (BigDecimal(size) / GB, "GB")
+                } else if (size >= 2 * MB) {
+                    (BigDecimal(size) / MB, "MB")
+                } else if (size >= 2 * KB) {
+                    (BigDecimal(size) / KB, "KB")
+                } else {
+                    (BigDecimal(size), "B")
+                }
+            }
+            "%.1f %s".formatLocal(Locale.US, value, unit)
+        }
+    }
+
+    def getRDDSize(rdd: RDD[InternalRow]) : Long = {
+        var rddSize = 0L
+        val rows = rdd.collect()
+        for (k <- rows.indices) {
+            val row = rows(k)
+            val len = row.numFields
+//            rddSize += SizeEstimator.estimate(Seq.fill(row.numFields)("12345").map { value => value.asInstanceOf[AnyRef] })
+            rddSize += SizeEstimator.estimate(row)
+        }
+        rddSize
+    }
     /**
      * Recurse all subtrees of the logical plan, updates the count for each partial QEP
      *
@@ -299,6 +437,48 @@ object Main {
     def getPartialQEPs(lp: LogicalPlan): Set[LogicalPlan] = {
         var subTrees: Set[LogicalPlan] = Set();
         subTreeCount(lp) = subTreeCount.getOrElse[Long](lp, 0) + 1
+
+        println("===== Input partial QEP =====")
+        println(lp)
+        val res = sparkSession.sessionState.executePlan(lp).toRdd
+        val calc_rowcount = res.count()
+        val calc_bytes = getRDDSize(res)
+
+
+//        res.cache.foreach((_: Row) => _ )
+//        val catalyst_plan = sparkSession.sessionState.executePlan(lp).logical
+        val est_bytes = sparkSession.sessionState.executePlan(
+            lp).optimizedPlan.stats.sizeInBytes
+        val est_rowcount = sparkSession.sessionState.executePlan(
+            lp).optimizedPlan.stats.rowCount.getOrElse(None)
+
+//        var sizeInBytes = "n/a";
+//        var rowCount = "n/a";
+//
+//        val statsPattern = "(?<=Statistics\\()(.*)(?=\\))".r
+//        val bytesPattern = "(?<=sizeInBytes=)(.*?)(,|$)".r
+//        val rowCountPattern = "(?<=rowCount=)(.*?)(,|$)".r
+//        val coststring = sparkSession.sessionState.executePlan(lp).explainString(CostMode)
+//        println(coststring)
+//        val matchStats = statsPattern.findFirstIn(coststring)
+//
+//        if (matchStats.isDefined) {
+//            val statistics = matchStats.get
+//            val matchBytes = bytesPattern.findFirstIn(statistics)
+//            val matchRowCount =  rowCountPattern.findFirstIn(statistics)
+//            if (matchBytes.isDefined){
+//                val bytesString = matchBytes.get
+//                sizeInBytes = bytesString.replace(",", "")
+//            }
+//            if (matchRowCount.isDefined){
+//                val rowCountString = matchRowCount.get
+//                rowCount = rowCountString.replace(",", "")
+//            }
+//        }
+
+        println("est sizeInBytes=" + bytesToString(est_bytes) + ", calc sizeInBytes=" + bytesToString(calc_bytes) + ", est rowCount=" + est_rowcount + ", calc rowCount=" + calc_rowcount)
+//        println("sizeInBytes=" + sizeInBytes + ", rowCount=" + rowCount)
+
         subTrees += lp
         if (lp.children.nonEmpty) {
             lp.children.foreach(x => {
@@ -359,6 +539,16 @@ object Main {
         }}
 
         uniques.close()
+
+        val comptime: BufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_DIR + DATA_SET + "_output_comptime_"+ NUM_DUMMY_ROWS +"rows_" + TOP_K + ".csv"))
+        val csvWriterComptime: CSVWriter = new CSVWriter(comptime, ';')
+        csvWriterComptime.writeNext("partialQEPId", "computationTime")
+
+        subTreeComputationTime foreach {case (key, value) =>
+            csvWriterComptime.writeNext(value.toString, key.toString)
+        }
+
+        comptime.close()
 
 
         val dependencyGraph: BufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_DIR + DATA_SET + "_output_dependency_graph_" + TOP_K + ".csv"))
